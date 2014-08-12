59b16fa860d98bc5005a22e80fb4989e57c34b6c
Sam van Kampen <sam@tehsvk.net>
2014-08-12 10:49:36 -0400

    Switch to monolithic notice listener
    There should probably be an explanation here, but I honestly don't know
    what to say.
    
     plugins/infobot.py | 136 ++++++++++++++++++++++++++---------------------------
     1 file changed, 66 insertions(+), 70 deletions(-)

--- plugins/infobot.py
+++ plugins/infobot.py
@@ -4,82 +4,80 @@ from functools import wraps
 from .util.data import get_doc
 from functools import partial
 import re
+import traceback
+import inspect
+import gc
 
 db = None
 
-old_callbacks = None
 pending_adds = {}
 pending_alias_adds = {}
 pending_appends = {}
+pending_rmalias = []
+pending_rminfo = []
 
-def insert_callback(bot, ctype, cback):
-    global old_callbacks
-    old_callbacks = bot.__irccallbacks__.get(ctype, None)
-    bot.register_callback(ctype, cback)
+def caller():
+    code_obj = inspect.stack()[1][0].f_code
+    referrers = [x for x in gc.get_referrers(code_obj) if inspect.isfunction(x)]
+    return referrers[0]
 
-def restore_callbacks(bot, ctype):
-    if old_callbacks == None:
-        del bot.__irccallbacks__[ctype]
+def insert_callback(bot, ctype, func):
+    try:
+        bot.__irccallbacks__[ctype].append(func)
+    except KeyError:
+        bot.__irccallbacks__[ctype] = [func]
+
+def notice_listener(bot, msg):
+    message = msg["arg"].split(" ", 1)[1][1:]
+    sender = msg["host"].split("!")[0]
+
+    if sender.lower() != "nickserv" or "ACC" not in message:
         return
-    bot.__irccallbacks__[ctype] = old_callbacks
+    if "ACC" in message and not message.endswith("3"):
+        nick = message.split()[0]
+        pending_adds.pop(nick, None)
+        pending_alias_adds.pop(nick, None)
+        pending_appends.pop(nick, None)
+        try:
+            pending_rmalias.remove(nick)
+        except ValueError:
+            pass
+        try:
+            pending_rminfo.remove(nick)
+        except ValueError:
+            pass
 
-def nlistener_base(funct):
-    @wraps(funct)
-    def new_func(bot, msg):
-        message = msg["arg"].split(" ", 1)[1][1:]
-        sender = msg["host"].split("!")[0]
-
-        if sender.lower() == 'nickserv' and message.endswith("ACC 3"):
-            funct(bot, msg, message, sender)
-    return new_func
-
-@nlistener_base
-def add_notice_listener(bot, msg, message, sender):
     nick = message.split()[0]
-    user, host, info = pending_adds[nick]
-    db.execute("SELECT addinfo(%s, %s, %s, %s);", (nick, user, host, info))
-    del pending_adds[nick]
-    bot.notice(nick, "Info set to '%s'" % (info))
-    restore_callbacks(bot, "NOTICE")
-
-@nlistener_base
-def addalias_notice_listener(bot, msg, message, sender):
-    nick = message.split()[0]
-    alias = pending_alias_adds[nick]
-    success = db.execute("SELECT addalias(%s, %s);", (nick, alias)).fetchone()[0]
-    if not success:
-        bot.notice(nick, "Error setting alias; you are creating an"
-            " infinitely looping alias chain.")
-    else:
-        bot.notice(nick, "The info of your current nick %s now points to %s." % (nick, alias))
-    del pending_alias_adds[nick]
-    restore_callbacks(bot, "NOTICE")
-
-@nlistener_base
-def rmalias_notice_listener(bot, msg, message, sender):
-    nick = message.split()[0]
-    db.execute("SELECT delalias(%s);", (nick,))
-    bot.notice(nick, "Your nick now points to itself instead of to an alias.")
-    restore_callbacks(bot, "NOTICE")
-
-@nlistener_base
-def rminfo_notice_listener(bot, msg, message, sender):
-    nick = message.split()[0]
-    db.execute("SELECT delinfo(%s);", (nick,))
-    bot.notice(nick, "Deleted info.")
-    restore_callbacks(bot, "NOTICE")
-
-@nlistener_base
-def append_notice_listener(bot, msg, message, sender):
-    nick = message.split()[0]
-    curinfo = db.execute("SELECT info FROM info(%s)", (nick,)).fetchone()[0]
-    user, host, info = pending_appends[nick]
-    curinfo += (" " + info)
-    db.execute("SELECT addinfo(%s, %s, %s, %s);", (nick, user, host, curinfo))
-    bot.notice(nick, "Info set to '%s'" % (curinfo))
-    del pending_appends[nick]
-    restore_callbacks(bot, "NOTICE")
-
+    if nick in pending_adds:
+        user, host, info = pending_adds[nick]
+        db.execute("SELECT addinfo(%s, %s, %s, %s);", (nick, user, host, info))
+        del pending_adds[nick]
+        bot.notice(nick, "Info set to '%s'" % (info))
+    if nick in pending_appends:
+        user, host, toappend = pending_appends[nick]
+        alias, info = db.execute("SELECT nick, info FROM info(%s)", (nick,)).fetchone()
+        info += (" " + toappend)
+        db.execute("SELECT addinfo(%s, %s, %s, %s);", (alias, user, host, info))
+        bot.notice(nick, "Info set to '%s'" % (info))
+        del pending_appends[nick]
+    if nick in pending_alias_adds:
+        alias = pending_alias_adds[nick]
+        success = db.execute("SELECT addalias(%s, %s);", (nick, alias)).fetchone()[0]
+        if not success:
+            bot.notice(nick, "Error setting alias; you are creating an"
+                " infinitely looping alias chain.")
+        else:
+            bot.notice(nick, "The info of your current nick %s now points to %s." % (nick, alias))
+        del pending_alias_adds[nick]
+    if nick in pending_rmalias:
+        pending_rmalias.remove(nick)
+        db.execute("SELECT delalias(%s);", (nick,))
+        bot.notice(nick, "Your nick now points to itself instead of to an alias.")
+    if nick in pending_rminfo:
+        pending_rminfo.remove(nick)
+        db.execute("SELECT delinfo(%s);", (nick,))
+        bot.notice(nick, "Deleted info.")
+   
 def addinfo(bot, pmsg):
     nick, chan, msg = process_privmsg(pmsg)
     
@@ -95,19 +93,18 @@ def addinfo(bot, pmsg):
     info = msg.split(" ", 1)[1]
     if 'alias' in info:
         pending_alias_adds[nick] = msg.split()[2]
-        insert_callback(bot, "NOTICE", addalias_notice_listener)
     else:
         pending_adds[nick] = (user, host, info)
-        insert_callback(bot, "NOTICE", add_notice_listener)
     bot._msg("NickServ", "ACC %s" % (nick))
 
-__callbacks__ = {"PRIVMSG": [addinfo]}
+__callbacks__ = {"PRIVMSG": [addinfo], "NOTICE": [notice_listener]}
 
 @init
 def init(bot):
     global db
     db = Database(password=bot.config["dbpass"])
     bot.data["db"] = db
+
 
 @command('info', '^(!|@)$name(\s|$)')
 def getinfo(bot, nick, chan, gr, arg):
@@ -133,9 +130,9 @@ def rmalias(bot, nick, chan, _, arg):
     if not arg or arg not in ('alias', 'info'):
         return bot._msg(chan, get_doc())
     if arg == 'alias':
-        insert_callback(bot, "NOTICE", rmalias_notice_listener)
+        pending_rmalias.append(nick)
     else:
-        insert_callback(bot, "NOTICE", rminfo_notice_listener)
+        pending_rminfo.append(nick)
     bot._msg("NickServ", "ACC %s" % (nick))
 
 @command('append', r'^!$name(?:\s|$)', ppmsg=True)
@@ -148,7 +145,6 @@ def appendinfo(bot, nick, chan, arg, pmsg):
     user = user.split("!")[0]
     
     pending_appends[nick] = (user, host, arg)
-    insert_callback(bot, "NOTICE", append_notice_listener)
     bot._msg("NickServ", "ACC %s" % (nick))
 
 @command('sql', '^&$name .+', admin=True)
