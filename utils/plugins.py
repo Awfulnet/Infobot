78981945dc09dba17749c3fd6f189b42a420f606
Sam van Kampen <sam@tehsvk.net>
2016-06-05 00:41:03 +0200

    [plugloader] Add PluginLoader class

---
+++ utils/plugins.py
@@ -0,0 +1,55 @@
+"""
+utils.plugins - Plugin loading functionality
+"""
+
+import re
+import fnmatch
+import os
+import importlib.util
+
+from collections import OrderedDict
+from importlib.machinery import SourceFileLoader
+
+DEPENDS_RE = re.compile(r"\* depends: (.+)")
+
+class PluginLoader(object):
+    """
+    The plugin loader
+    Generates a dependency graph and returns a list of loaded modules
+    when load_all() is called.
+    """
+    def __init__(self):
+        """ Initializes the PluginLoader by generating a dependency graph """
+        self.directory = './plugins'
+        self.graph = {}
+        files = fnmatch.filter(os.listdir('./plugins'), "*.py")
+        for f in files:
+            for line in open(os.path.join(self.directory, f)):
+                match = DEPENDS_RE.search(line)
+                if match:
+                    self.graph[f] = match.group(1).split(', ')
+            if f not in self.graph and f != '__init__.py':
+                # This plugin does not yet define a dependency line, and therefore has no dependencies
+                self.graph[f] = []
+
+    def load_plugin(self, plugin_filename, name=None):
+        if name is None:
+            name = "plugins." + plugin_filename[:-3]
+        plugin = SourceFileLoader(name, os.path.join(self.directory, plugin_filename)).load_module()
+        return plugin
+
+    def load_all(self):
+        plugins = []
+        self.load_plugin('__init__.py', name='plugins')
+        while (self.graph):
+            satisfied_plugins = dict(filter(lambda x: not x[1], self.graph.items()))
+            for satisfied_plugin, _ in satisfied_plugins.items():
+                plugins.append(self.load_plugin(satisfied_plugin))
+                del self.graph[satisfied_plugin]
+                for plugin, deps in self.graph.items():
+                    # This try clause is here because .remove errors when the item is not in the list
+                    try:
+                        deps.remove(satisfied_plugin[:-3])
+                    except ValueError:
+                        pass
+        return plugins
