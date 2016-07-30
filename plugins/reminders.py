48d7f2bbf8d3cb93c6fa5a6b864e2577fca5a786
Sam van Kampen <sam@tehsvk.net>
2016-07-30 12:38:50 +0000

    Merge and clean up reminders

--- plugins/reminders.py
+++ plugins/reminders.py
@@ -6,50 +6,77 @@
 import parsedatetime
 import datetime
 import humanize
+import regex
+import threading
+import logging
 
+from pytz import timezone
 from .util.decorators import command, init, process_privmsg
 from collections import namedtuple, defaultdict
 
-db = None
-Tell = namedtuple("Tell", ["id", "from_nick", "message", "date", "fulfilled"])
+logger = logging.getLogger("remind")
+logger.setLevel(logging.DEBUG)
 
+db = None
+Tell = namedtuple("Tell", ["id", "from_nick", "message", "date"])
+Reminder = namedtuple("Reminder", ["id", "to_nick", "from_nick", "message", "channel", "begints", "endts"])
+
+calendar = parsedatetime.Calendar()
+
+timers = []
+reminders = defaultdict(list)
 tells = defaultdict(list)
+
+REMIND_RE = "\.remind (me|@?[A-z0-9]+) (?:in (.+?) to (.+)$|(?:to )?(.+?) in (.+)$)"
+
+REMIND_TELL_RE = regex.compile(r"""
+    \.(?:tell|remind)\s
+        (?P<nick> me|@? [A-z0-9]+)\s
+        (?:
+            (?:in\s(?P<time> .+?))\s(?:to \s (?P<message>.+)$)
+            |
+            (?:
+                (?:to \s )?
+                    (?:(?P<message>.+)\sin\s(?P<time>.+)
+                    |
+                    (?P<message>.+)
+                )
+            )
+        )""",
+        regex.X | regex.V1)
 
 @init
 def init(bot):
     global db
     db = bot.data["db"]
+    db_get_tells()
+    db_get_reminders(bot)
+
+def add_tell(to_nick, tellid, from_nick, message, begints):
+    tells[to_nick].append(
+        Tell(tellid, from_nick, message, begints))
+
+def add_reminder(bot, *args):
+    reminder = Reminder(*args)
+    delta = reminder.endts - datetime.datetime.utcnow()
+    if (delta.total_seconds() < 0):
+        return
+
+    timer = threading.Timer(delta.total_seconds(), remind_handler, args=(bot, reminder))
+    timer.start()
+    timers.append(timer)
+    reminders[reminder.to_nick].append(reminder)
+    logger.debug("Added reminder %r (fires in %f seconds)", reminder, delta.total_seconds())
+
+def db_get_tells():
     data = db.execute("SELECT to_nick, tellid, from_nick, message, begints FROM tells WHERE fulfilled = false;").fetchall()
     for tell in data:
-        tells[tell[0]].append(Tell(tell[1], tell[2], tell[3], tell[4], False))
+        add_tell(*tell)
 
-def tell_handler(bot, pmsg):
-    nick, chan, msg = process_privmsg(pmsg)
-
-    if nick in tells:
-        for tell in tells[nick]:
-            send_tell(bot, nick, chan, tell)
-            fulfill_db(tell.id)
-        del tells[nick]
-
-__callbacks__ = {"PRIVMSG": [tell_handler]}
-
-@command('tell', r'^\.tell .+')
-def tell(bot, nick, chan, arg):
-    """ .tell <nick> <message> - Tells <nick> your message when they are next seen """
-    args = arg.split(" ",1)
-    db = bot.data["db"]
-    if len(args) < 2:
-        return bot._msg(chan, get_doc())
-
-    to_nick, message = args
-    date = datetime.datetime.utcnow()
-
-    tellid = db.execute("INSERT INTO tells (to_nick, from_nick, message) VALUES (%s,%s,%s) RETURNING tellid;",
-               (to_nick, nick, message)).fetchone()[0];
-
-    tells[to_nick].append(Tell(tellid, nick, message, date, False))
-    bot.msg(chan, "I'll tell them that.")
+def db_get_reminders(bot):
+    data = db.execute("SELECT id, to_nick, from_nick, message, channel, begints, endts FROM reminders WHERE fulfilled = false;").fetchall()
+    for reminder in data:
+        add_reminder(bot, *reminder)
 
 def send_tell(bot, nick, chan, tell):
     timestring = humanize.naturaltime(datetime.datetime.utcnow() - tell.date)
@@ -63,6 +90,65 @@ def send_tell(bot, nick, chan, tell):
     )
     bot.notice(chan, output)
 
-def fulfill_db(tellid):
-    db.execute("UPDATE tells SET fulfilled = true WHERE tellid = %s;", (tellid))
+    db.execute("UPDATE tells SET fulfilled = true WHERE tellid = %s;", (tell.id))
 
+def tell_handler(bot, pmsg):
+    nick, chan, msg = process_privmsg(pmsg)
+
+    if nick in tells:
+        for tell in tells[nick]:
+            send_tell(bot, nick, chan, tell)
+        del tells[nick]
+
+def remind_handler(bot, reminder):
+    timestring = humanize.naturaltime(datetime.datetime.utcnow() - reminder.begints)
+    output = "{} {}: {} · {} · {}".format(
+        bot.style.green("| ✉ |"),
+        bot.style.teal(reminder.to_nick),
+        bot.style.teal(reminder.message),
+        bot.style.teal("from {}".format(reminder.from_nick)),
+        bot.style.teal("\u231A {}".format(timestring))
+    )
+
+    bot.notice(reminder.channel, output)
+
+    db.execute("UPDATE reminders SET fulfilled = true WHERE id = %s;", (reminder.id,))
+
+__callbacks__ = {"PRIVMSG": [tell_handler]}
+
+@command('remind', REMIND_TELL_RE)
+def remind(bot, nick, chan, gr, arg):
+    to_nick = gr['nick']
+    time = gr['time']
+    message = gr['message']
+    if (to_nick == 'me'):
+        to_nick = nick
+
+    to_nick = to_nick.strip('@')
+
+    if (time is not None):
+        # this is a reminder, as it has an end time
+        endts = calendar.parseDT(time, tzinfo=timezone("UTC"))[0]
+        # UTCnow doesn't add a timezone attribute, so we have to add it ourselves
+        utcnow = datetime.datetime.utcnow().replace(tzinfo=timezone("UTC"))
+        delta = endts - utcnow
+
+        reminderid = db.execute("INSERT INTO reminders (from_nick, to_nick, message, channel, endts) VALUES (%s,%s,%s,%s,%s) RETURNING id;",
+                                (nick, to_nick, message, chan, endts)).fetchone()[0]
+
+        reminder = Reminder(reminderid, to_nick, nick, message, chan, datetime.datetime.utcnow(), endts)
+
+        timer = threading.Timer(delta.total_seconds(), remind_handler, args=(bot, reminder))
+        timer.start()
+        timers.append(timer)
+
+        reminders[to_nick].append(reminder)
+        bot.msg(chan, "I'll remind them.")
+    else:
+        # this is a tell
+        tellid = db.execute("INSERT INTO tells (to_nick, from_nick, message) VALUES (%s,%s,%s) RETURNING tellid;",
+                (to_nick, nick, message)).fetchone()[0];
+
+        tells[to_nick].append(Tell(tellid, nick, message, datetime.datetime.utcnow()))
+
+        bot.msg(chan, "I'll tell them that.")
