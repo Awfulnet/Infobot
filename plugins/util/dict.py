71bbe09c9e3229ae688089c0bfa444e7c97209be
Sam van Kampen <sam@tehsvk.net>
2016-08-05 21:38:39 +0200

    [tests/CIDD] Add Case-Insensitive Case-Preserving DefaultDict
    This is the first class using the new testing infrastructure.
    Here's to more of those!

---
+++ plugins/util/dict.py
@@ -0,0 +1,55 @@
+import copy
+
+class CaseInsensitiveDefaultDict(dict):
+    @classmethod
+    def _k(cls, key):
+        return key.casefold() if isinstance(key, str) else key
+
+    def __init__(self, default=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.default = default
+
+    def __getitem__(self, toMatch):
+        match = None
+        for key, value in self.items():
+            if (self._k(key) == self._k(toMatch)):
+                match = value
+        if (match is None and self.default is not None):
+            _copy = copy.deepcopy(self.default)
+            super().__setitem__(toMatch, _copy)
+            return _copy
+
+        if (match is None):
+            raise KeyError
+
+        return match
+
+    def __setitem__(self, key, value):
+        for k, v in self.items():
+            if (self._k(k) == self._k(key)):
+                self.__setitem__(k, value)
+                return
+        super().__setitem__(key, value)
+
+    def __delitem__(self, key, value):
+        for k, v in self.items():
+            if (self._k(k) == self._k(key)):
+                super().__delitem__(k)
+
+    def __contains__(self, key):
+        for k, v in self.items():
+            if (self._k(k) == self._k(key)):
+                return True
+        return False
+
+    def pop(self, key, *args, **kwargs):
+        for k, v in self.items():
+            if (self._k(k) == self._k(key)):
+                return super().pop(k, *args, **kwargs)
+        return super().pop(key, *args, **kwargs)
+
+    def get(self, key, *args, **kwargs):
+        for k, v in self.items():
+            if (self._k() == self._k(key)):
+                return super().get(k, *args, **kwargs)
+        return super().get(key, *args, **kwargs)
---
+++ test/test_CIDD.py
@@ -0,0 +1,38 @@
+import unittest
+from plugins.util.dict import CaseInsensitiveDefaultDict as CIDD
+
+class TestCIDD(unittest.TestCase):
+    def testDefaultGet(self):
+        """
+        This test checks whether the dict behaves as a defaultdict
+        """
+        sample = CIDD(default=[])
+        self.assertEqual(sample["ABC"], [])
+
+    def testDefaultCopy(self):
+        """
+        This test checks whether the provided default value gets
+        copied properly.
+        """
+        sample = CIDD(default=[])
+        sample["abc"].append(42)
+        self.assertNotEqual(sample["abc"], sample.default)
+
+    def testCaseInsensitivity(self):
+        """
+        This test checks whether keys are compared
+        insensitively.
+        """
+        sample = CIDD()
+        sample["abc"] = 42
+        self.assertEqual(sample["aBc"], 42)
+
+    def testCasePreservation(self):
+        """
+        This test checks whether key case is preserved
+        internally.
+        """
+        sample = CIDD()
+        sample["aBc"] = 42
+        self.assertEqual(sample, {"aBc": 42})
+        self.assertNotEqual(sample, {"abc": 42})
