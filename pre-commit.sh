3331d9fa08e9824067daab9b5e1239a8aaf72d1b
Sam van Kampen <sam@tehsvk.net>
2016-08-05 21:37:26 +0200

    Add unit testing and static analysis support
    This adds support for unit testing through the `unittest` framework
    and static analysis through use of pyflakes. Another addition is the
    pre-commit.sh bash script. This contains the infrastructure for running
    tests and static analysis. This is intended to be run before anything
    is committed to the repo - please use it by symlinking it to .git/hooks
    as follows:
    
        ln -s ../../pre-commit.sh .git/hooks/pre-commit

---
+++ pre-commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd $(git rev-parse --show-toplevel)
+for i in $(git diff --name-only --staged | grep -E '\.py$')
+do
+	printf "Analyzing ${i} ... "
+	git show ":$i" | pyflakes
+	if [ $? -eq 1 ]
+	then
+		echo "error"
+		echo "Error during pyflakes execution, aborting..."
+		exit 1
+	fi
+	echo "ok"
+done
+
+if [ -d "test" ]; then
+	python -m unittest discover -s test
+	if [ $? -eq 1 ]
+	then
+		echo "Error during unit tests, aborting..."
+		exit 1
+	fi
+fi
