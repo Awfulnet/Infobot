e5ca8fa80235461582d6923a4aad2f35d4b82057
Sam van Kampen <sam@tehsvk.net>
2018-08-11 22:54:33 +0000

    Add import_merge for package plugins

---
+++ plugins/util/__init__.py
@@ -0,0 +1,41 @@
+import importlib
+from operator import add
+from pathlib import Path
+import sys
+from collections import defaultdict
+from functools import reduce
+
+def _merge_listdict_single(acc, x):
+    """ Merges two dictionaries which contain lists.
+        Initial value (acc) needs to be a defaultdict. """
+    for k, v in x.items():
+        acc[k] += v
+    return acc
+
+def import_merge(name):
+    """ Imports all single-level submodules from a given module name.
+        Additionally, merges __callbacks__ and __inits__. """
+    module = sys.modules[name]
+    modpath = Path(module.__path__[0])
+    package = module.__package__
+
+    print(package)
+
+    to_load = []
+    for subpath in modpath.iterdir():
+        if subpath.name.endswith('.py') and subpath.name != '__init__.py':
+            to_load.append('.'+subpath.stem)
+
+    mods = [importlib.import_module(name, package=package) for name in to_load]
+
+    merged_callbacks = reduce(_merge_listdict_single,
+                              (getattr(m, '__callbacks__', {}) for m in mods),
+                              defaultdict(list))
+
+    merged_inits     = reduce(add, (getattr(m, '__inits__', []) for m in mods))
+
+    setattr(module, '__callbacks__', merged_callbacks)
+    setattr(module, '__inits__', merged_inits)
+
+    for mod in mods:
+        setattr(module, mod.__name__, mod)
